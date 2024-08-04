c64ea44404a6d4e536ae562833fa42b390c183f9
harsh-kanjar <harshlankar07@gmail.com>
2024-08-04 17:06:28 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
